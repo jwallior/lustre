lustre/lustre-release

commit 60c5bc2502591f46260e11db540c0ec2adbc8db8

LU-8235 scripts: pass unrecognized options to lfs migrate
Pass through any unrecognized options to the "lfs migrate" command,
allowing the script to support migrate options without any special
handling.

Add new options "--rsync" and "--no-rsync" to specify how rsync
is used as a fallback alternative for the "lfs migrate" command.
The "--rsync" option forces the use of rsync instead of lfs migrate,
while the "--no-rsync" option prevents falling back to rsync in
the case where lfs migrate fails.

Add new "--dry-run" option.  The current "-n" option for a dry-run
duplicates the "-n" option to designate non-block for "lfs migrate".
The script's usage of "-n" is therefore deprecated, so that a future
patch can instead pass it through.

Add new "-v" option to increase verbosity, to help test/debug/monitor
what is being done by the script.  This option is also passed
through to "lfs migrate".

Test-Parameters: trivial
Signed-off-by: Nathan Dauchy <Nathan.Dauchy@noaa.gov>
Signed-off-by: Steve Guminski <stephenx.guminski@intel.com>
Change-Id: Ia3f56bf528d2ac8155f08d93d01abd8bb9168cc4
Reviewed-on: https://review.whamcloud.com/20621
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: John L. Hammond <john.hammond@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

lustre/scripts/lfs_migrate
@@ -16,10 +16,9 @@ RSYNC=${RSYNC:-rsync}
 LFS_MIGRATE_RSYNC_MODE=${LFS_MIGRATE_RSYNC_MODE:-false}
 ECHO=echo
 LFS=${LFS:-lfs}
-LFS_MIGRATE_RSYNC=${LFS_MIGRATE_RSYNC:-false}
 RSYNC_WITH_HLINKS=false
 LFS_MIGRATE_TMP=${TMPDIR:-/tmp}
-MIGRATED_SET="$(mktemp ${LFS_MIGRATE_TMP}/lfs_migrate-$$.links.XXXXXX)"
+MIGRATED_SET="$(mktemp ${LFS_MIGRATE_TMP}/lfs_migrate.links.XXXXXX)"
 NEWNAME=""
 REMOVE_FID='s/^\[[0-9a-fx:]*\] //'
 
@@ -45,27 +44,33 @@ old_fid_in_set() {
 
 usage() {
     cat -- <<USAGE 1>&2
-usage: lfs_migrate [-c <stripe_count>] [-h] [-n] [-q] [-R] [-s]
-                   [-S <stripe_size>] [-y] [-0] [file|dir ...]
-    -c <stripe_count>
-       restripe file using the specified stripe count
-    -h show this usage message
-    -n only print the names of files to be migrated
-    -q run quietly (don't print filenames or status)
-    -R restripe file using default directory striping
-    -s skip file data comparison after migrate
-    -S <stripe_size>
-       restripe file using the specified stripe size
-    -y answer 'y' to usage question
-    -0 input file names on stdin are separated by a null character
+usage: lfs_migrate [--dry-run] [-h] [--no-rsync|--rsync] [-q] [-R] [-s]
+		   [-v] [-y] [-0] [FILE|DIR...]
+	--dry-run only print the names of files to be migrated
+	-h show this usage message
+	--no-rsync do not fall back to rsync mode even if lfs migrate fails
+	-q run quietly (don't print filenames or status)
+	--rsync force rsync mode instead of using lfs migrate
+	-R restripe file using default directory striping
+	-s skip file data comparison after migrate
+	-v show verbose debug messages
+	-y answer 'y' to usage question
+	-0 input file names on stdin are separated by a null character
 
 The -c <stripe_count> and -S <stripe_size> options may not be specified at
 the same time as the -R option.
 
+The --rsync and --no-rsync options may not be specified at the same time.
+
 If a directory is an argument, all files in the directory are migrated.
 If no file/directory is given, the file list is read from standard input.
 
-e.g.: lfs_migrate /mnt/lustre/dir
+Any arguments that are not explicitly recognized by the script are passed
+through to the 'lfs migrate' utility.
+
+Examples:
+      lfs_migrate /mnt/lustre/dir
+      lfs_migrate -p newpool /mnt/lustre/dir
       lfs find /test -O test-OST0004 -size +4G | lfs_migrate -y
 USAGE
     exit 1
@@ -78,35 +83,124 @@ cleanup() {
 
 trap cleanup EXIT
 
-OPT_CHECK=y
-OPT_STRIPE_COUNT=""
-OPT_STRIPE_SIZE=""
+OPT_CHECK=true
+OPT_DEBUG=false
+OPT_NO_RSYNC=false
+OPT_DRYRUN=false
+OPT_YES=false
+OPT_RESTRIPE=false
+OPT_NULL=false
+OPT_PASSTHROUGH=()
+STRIPE_COUNT=""
+STRIPE_SIZE=""
+POOL=""
 
-while getopts "c:hlnqRsS:y0" opt $*; do
+# Examine any long options and arguments.  getopts does not support long
+# options, so they must be stripped out and classified as either options
+# for the script, or passed through to "lfs migrate".
+LONG_OPT=false
+SHORT_OPT=false
+OPTS=()
+
+for f in $(seq 1 $#); do
+	arg=${!f}
+	if [ "${arg:0:2}" = "--" ]; then
+		SHORT_OPT=false
+		if [ "$arg" = "--block" ]; then
+			BLOCK="$arg"
+			OPT_YES=true
+		elif [ "$arg" = "--non-block" ]; then
+			BLOCK="$arg"
+		elif [ "$arg" = "--dry-run" ]; then
+			OPT_DRYRUN=true
+			OPT_YES=true
+		elif [ "$arg" = "--rsync" ]; then
+			LFS_MIGRATE_RSYNC_MODE=true
+		elif [ "$arg" = "--no-rsync" ]; then
+			OPT_NO_RSYNC=true
+			OPT_YES=true
+		else
+			LONG_OPT=true
+			OPT_PASSTHROUGH+=("$arg")
+		fi
+	elif [ "${arg:0:1}" = "-" ]; then
+		LONG_OPT=false
+		if [ "$arg" == "-b" ]; then
+			BLOCK="$arg"
+		else
+			SHORT_OPT=true
+			OPTS+=("$arg")
+		fi
+	elif $LONG_OPT; then
+		LONG_OPT=false
+		# This will prevent long options from having file name
+		# arguments, but allows long options with no arguments to work.
+		if [ -f "$arg" -o -d "$arg" ]; then
+			OPTS+=("$arg")
+		else
+			[ "${OPT_PASSTHROUGH[-1]}" = "--stripe-count" ] &&
+				STRIPE_COUNT="$arg"
+			[ "${OPT_PASSTHROUGH[-1]}" = "--stripe-size" ] &&
+				STRIPE_SIZE="$arg"
+			[ "${OPT_PASSTHROUGH[-1]}" = "--pool" ] &&
+				POOL="$arg"
+			OPT_PASSTHROUGH+=("$arg")
+		fi
+	elif $SHORT_OPT; then
+		[ "${OPTS[-1]}" = "-c" ] &&
+			STRIPE_COUNT="$arg"
+		[ "${OPTS[-1]}" = "-S" ] &&
+			STRIPE_SIZE="$arg"
+		[ "${OPTS[-1]}" = "-p" ] &&
+			POOL="$arg"
+		SHORT_OPT=false
+		OPTS+=("$arg")
+	else
+		OPTS+=("$arg")
+	fi
+done
+
+# Reset the argument list to include only the short options and file names
+set -- "${OPTS[@]}"
+
+while getopts ":hlnqRsvy0" opt $*; do
     case $opt in
-	c) OPT_STRIPE_COUNT=$OPTARG;;
+	h) usage;;
 	l) ;; # maintained for backward compatibility
-	n) OPT_DRYRUN=n; OPT_YES=y;;
+	n) OPT_DRYRUN=true
+	   OPT_YES=true
+	   echo "$(basename $0): -n deprecated, use --dry-run instead" 1>&2
+	   echo "$(basename $0): to specify non-block, use --non-block instead" 1>&2;;
 	q) ECHO=:;;
-	R) OPT_RESTRIPE=y;;
-	s) OPT_CHECK="";;
-	S) OPT_STRIPE_SIZE=$OPTARG;;
-	y) OPT_YES=y;;
-	0) OPT_NULL=y;;
-	h|\?) usage;;
+	R) OPT_RESTRIPE=true;;
+	s) OPT_CHECK=false;;
+	v) OPT_DEBUG=true; ECHO=echo; OPT_PASSTHROUGH+=("-v");;
+	y) OPT_YES=true;;
+	0) OPT_NULL=true;;
+	*) # Pass through any unrecognized options to 'lfs migrate'
+	   OPT_PASSTHROUGH+=("-$OPTARG")
+	   if [[ ${!OPTIND:0:1} != "-" && ! -f "${!OPTIND}" &&
+		 ! -d "${!OPTIND}" ]]; then
+		OPT_PASSTHROUGH+=("${!OPTIND}")
+		((OPTIND++))
+	   fi;;
     esac
 done
 shift $((OPTIND - 1))
 
-if [ -n "$OPT_STRIPE_COUNT""$OPT_STRIPE_SIZE" -a "$OPT_RESTRIPE" ]; then
-	echo ""
-	echo "$(basename $0) error: The -c <stripe_count> option and" 1>&2
-	echo "-S <stripe_size> option may not" 1>&2
-	echo "be specified at the same time as the -R option." 1>&2
+if $OPT_RESTRIPE && [[ "$STRIPE_COUNT" || "$STRIPE_SIZE" ]]; then
+	echo "$(basename $0): Options -c <stripe_count> and -S <stripe_size> "\
+	"may not be specified at the same time as the -R option." 1>&2
 	exit 1
 fi
 
-if [ -z "$OPT_YES" ]; then
+if $LFS_MIGRATE_RSYNC_MODE && $OPT_NO_RSYNC; then
+	echo "$(basename $0): Options --rsync and --no-rsync may not be "\
+	"specified at the same time." 1>&2
+	exit 1
+fi
+
+if ! $OPT_YES; then
 	echo ""
 	echo "lfs_migrate is currently NOT SAFE for moving in-use files." 1>&2
 	echo "Use it only when you are sure migrated files are unused." 1>&2
@@ -135,33 +229,40 @@ umask 0077
 lfs_migrate() {
 	while IFS='' read -d '' OLDNAME; do
 		local hlinks=()
+		local stripe_size="$STRIPE_SIZE"
+		local stripe_count="$STRIPE_COUNT"
+		local parent_count=""
+		local parent_size=""
+
+		$ECHO -n "$OLDNAME: "
 
 		# avoid duplicate stat if possible
-		local nlink_type=($(LANG=C stat -c "%h %F" "$OLDNAME" || true))
+		local nlink_type=($(LANG=C stat -c "%h %F" "$OLDNAME" 	\
+				 2> /dev/null))
 
 		# skip non-regular files, since they don't have any objects
 		# and there is no point in trying to migrate them.
 		if [ "${nlink_type[1]}" != "regular" ]; then
-			echo -e "$OLDNAME: not a regular file, skipped"
+			echo -e "\r\e[K$OLDNAME: not a regular file, skipped"
 			continue
 		fi
 
 		# working out write perms is hard, let the shell do it
 		if [ ! -w "$OLDNAME" ]; then
-			echo -e "$OLDNAME: no write permission, skipped"
+			echo -e "\r\e[K$OLDNAME: no write permission, skipped"
 			continue
 		fi
 
-		if [ "$OPT_DRYRUN" ]; then
-			echo -e "$OLDNAME: dry run, skipped"
+		if $OPT_DRYRUN && ! $OPT_DEBUG; then
+			$ECHO "dry run, skipped"
 			continue
 		fi
 
 		# xattrs use absolute file paths, so ensure provided path is
 		# also absolute so that the names can be compared
 		local oldname_absolute=$(readlink -f "$OLDNAME")
-		if [ $? -ne 0 ]; then
-			echo -e "$OLDNAME: cannot resolve full path"
+		if [ -z "$oldname_absolute" ]; then
+			echo -e "\r\e[K$OLDNAME: cannot resolve full path, skipped"
 			continue
 		fi
 		OLDNAME=$oldname_absolute
@@ -170,14 +271,14 @@ lfs_migrate() {
 		# should be replaced with a single call to
 		# "lfs path2links" once that command is available.  The logic
 		# for detecting unlisted hard links could then be removed.
-		local fid=$(lfs path2fid "$OLDNAME" 2> /dev/null)
+		local fid=$($LFS path2fid "$OLDNAME" 2> /dev/null)
 		if [ $? -ne 0 ]; then
-			echo -n "$OLDNAME: cannot determine FID; skipping; "
+			echo -n "\r\e[K$OLDNAME: cannot determine FID; skipping; "
 			echo "is this a Lustre file system?"
 			continue
 		fi
 
-		if [[ ${nlink_type[0]} -gt 1 || $RSYNC_WITH_HLINKS == true ]]; then
+		if [[ ${nlink_type[0]} -gt 1 ]] || $RSYNC_WITH_HLINKS; then
 			# don't migrate a hard link if it was already migrated
 			if path_in_set "$OLDNAME"; then
 				$ECHO -e "$OLDNAME: already migrated via another hard link"
@@ -192,7 +293,7 @@ lfs_migrate() {
 			local migrated=$(old_fid_in_set "$fid")
 			if [ -n "$migrated" ]; then
 				$ECHO -e "$OLDNAME: already migrated via another hard link"
-				if [[ $LFS_MIGRATE_RSYNC == true ]]; then
+				if $LFS_MIGRATE_RSYNC_MODE; then
 					# Only the rsync case has to relink.
 					# The lfs migrate case preserves the
 					# inode so the links are already
@@ -205,7 +306,7 @@ lfs_migrate() {
 			fi
 		fi
 
-		if [ "$OPT_RESTRIPE" ]; then
+		if $OPT_RESTRIPE; then
 			UNLINK=""
 		else
 		# if rsync copies Lustre xattrs properly in the future
@@ -213,22 +314,45 @@ lfs_migrate() {
 		# then we don't need to do this getstripe/mktemp stuff.
 			UNLINK="-u"
 
-			[ "$OPT_STRIPE_COUNT" ] &&
-				stripe_count=$OPT_STRIPE_COUNT ||
-				stripe_count=$($LFS getstripe -c "$OLDNAME" \
-					       2> /dev/null)
-			[ "$OPT_STRIPE_SIZE" ] &&
-				stripe_size=$OPT_STRIPE_SIZE ||
-				stripe_size=$($LFS getstripe -S \
-					      "$OLDNAME" 2> /dev/null)
+			[ -z "$stripe_count" ] &&
+				stripe_count=$($LFS getstripe -c "$OLDNAME" 2> /dev/null)
 
-			if [ -z "$stripe_count" -o -z "$stripe_size" ]; then
-				UNLINK=""
-				echo -e "$OLDNAME: cannot determine stripe info; skipping"
-				continue
+			[ -z "$stripe_size" ] &&
+				stripe_size=$($LFS getstripe -S "$OLDNAME" 2> /dev/null)
+
+			[ -z "$stripe_count" -o -z "$stripe_size" ] && UNLINK=""
+		fi
+
+		if $OPT_DEBUG; then
+			if $OPT_RESTRIPE; then
+				parent_count=$($LFS getstripe -c \
+					       $(dirname "$OLDNAME") 2> \
+					       /dev/null)
+				parent_size=$($LFS getstripe -S \
+					      $(dirname "$OLDNAME") 2> \
+					      /dev/null)
 			fi
-			stripe_size="-S$stripe_size"
-			stripe_count="-c$stripe_count"
+
+			$ECHO -n "stripe" \
+				"count=${stripe_count:-$parent_count}," \
+				"size=${stripe_size:-$parent_size}," \
+				"pool=${POOL:-not in a pool}: "
+		fi
+
+		if $OPT_DRYRUN; then
+			$ECHO "dry run, skipped"
+			continue
+		fi
+
+		if [[ "$stripe_count" && -z "$STRIPE_COUNT" ]]; then
+			stripe_count="-c $stripe_count"
+		else
+			stripe_count=""
+		fi
+		if [[ "$stripe_size" && -z "$STRIPE_SIZE" ]]; then
+			stripe_size="-S $stripe_size"
+		else
+			stripe_size=""
 		fi
 
 		# detect other hard links and store them on a global
@@ -236,56 +360,62 @@ lfs_migrate() {
 		local mntpoint=$(df -P "$OLDNAME" |
 				awk 'NR==2 { print $NF; exit }')
 		if [ -z "$mntpoint" ]; then
-			echo -e "$OLDNAME: cannot determine mount point; skipping"
+			echo -e "\r\e[K$OLDNAME: cannot determine mount point; skipped"
 			continue
 		fi
-		local hlinks=$(lfs fid2path "$mntpoint" "$fid" 2> /dev/null)
+		hlinks=$($LFS fid2path "$mntpoint" "$fid" 2> /dev/null)
 		if [ $? -ne 0 ]; then
-			echo -n "$OLDNAME: cannot determine hard link paths"
+			echo -e "\r\e[K$OLDNAME: cannot determine hard link paths, skipped"
 			continue
 		fi
 		hlinks+=("$OLDNAME")
 
 		# first try to migrate via Lustre tools, then fall back to rsync
-		if [[ $LFS_MIGRATE_RSYNC == false ]]; then
-			if $LFS migrate "$stripe_count" "$stripe_size" "$OLDNAME"; then
-				$ECHO -e "$OLDNAME: done migrate"
+		if ! $LFS_MIGRATE_RSYNC_MODE; then
+			if $LFS migrate "${OPT_PASSTHROUGH[@]}" ${BLOCK} \
+			   ${stripe_count} ${stripe_size} "$OLDNAME" &> \
+			   /dev/null; then
+				$ECHO "done migrate"
 				for link in ${hlinks[*]}; do
 					add_to_set "$fid" "$link"
 				done
 				continue
+			elif $OPT_NO_RSYNC; then
+				echo -e "\r\e[K$OLDNAME: refusing to fall back to rsync, skipped" 1>&2
+				continue
 			else
-				echo -e "$OLDNAME: falling back to rsync-based migration"
-				LFS_MIGRATE_RSYNC=true
+				$ECHO -n "falling back to rsync: "
+				LFS_MIGRATE_RSYNC_MODE=true
 			fi
 		fi
 
 		NEWNAME=$(mktemp $UNLINK "$OLDNAME-lfs_migrate.tmp.XXXXXX")
 		if [ $? -ne 0 -o -z "$NEWNAME" ]; then
-			echo -e "$OLDNAME: can't make temp file, skipped" 1>&2
+			echo -e "\r\e[K$OLDNAME: can't make temp file, skipped" 1>&2
 			continue
 		fi
 
-		[ "$UNLINK" ] && $LFS setstripe ${stripe_count}	\
-			${stripe_size} "$NEWNAME"
+		[ "$UNLINK" ] && $LFS setstripe ${OPT_PASSTHROUGH}	\
+				 ${stripe_count} ${stripe_size}		\
+				 "$NEWNAME" &> /dev/null
 
 		# we use --inplace, since we created our own temp file already
 		if ! $RSYNC -a --inplace $RSYNC_OPTS "$OLDNAME" "$NEWNAME";then
-			echo -e "$OLDNAME: copy error, exiting" 1>&2
+			echo -e "\r\e[K$OLDNAME: copy error, exiting" 1>&2
 			exit 4
 		fi
 
-		if [ "$OPT_CHECK" ] && ! cmp -s "$OLDNAME" "$NEWNAME"; then
-			echo -e "$NEWNAME: compare failed, exiting" 1>&2
+		if $OPT_CHECK && ! cmp -s "$OLDNAME" "$NEWNAME"; then
+			echo -e "\r\e[K$NEWNAME: compare failed, exiting" 1>&2
 			exit 8
 		fi
 
 		if ! mv "$NEWNAME" "$OLDNAME"; then
-			echo -e "$OLDNAME: rename error, exiting" 1>&2
+			echo -e "\r\e[K$OLDNAME: rename error, exiting" 1>&2
 			exit 12
 		fi
 
-		$ECHO -e "$OLDNAME: done migrate via rsync"
+		$ECHO "done migrate via rsync"
 		for link in ${hlinks[*]}; do
 			if [ "$link" != "$OLDNAME" ]; then
 				ln -f "$OLDNAME" "$link"
@@ -303,7 +433,7 @@ lfs_migrate() {
 }
 
 if [ "$#" -eq 0 ]; then
-	if [ "$OPT_NULL" ]; then
+	if $OPT_NULL; then
 		lfs_migrate
 	else
 		tr '\n' '\0' | lfs_migrate
@@ -311,11 +441,11 @@ if [ "$#" -eq 0 ]; then
 else
 	while [ "$1" ]; do
 		if [ -d "$1" ]; then
-			$LFS find "$1" -type f -print0 | lfs_migrate
+			$LFS find "$1" -type f -print0
 		else
-			echo -en "$1\0" | lfs_migrate
+			echo -en "$1\0"
 		fi
 		shift
-	done
+	done | lfs_migrate
 fi
 

jwallior/lustre

commit b58e82cdb7c890d6795f4b3dd7b5c0066f03fb9b

LU-12157 utils: fix lfs_migrate output and testing
Don't pass the "-v" option through to "lfs migrate", as this causes
the filename to be printed twice when run with the "-v" option.

Don't use "echo -e" to process escape characters in filenames unless
this is needed, but add it where it is needed.  Don't add ANSI escape
characters to the output.  The output previously looked like:

     /mnt/testfs/l0: stripe count=1,size=1048576,pool=/mnt/testfs/l0
     done migrate
     nr[K/mnt/testfs/l1: /mnt/testfs/l1: already migrated ...
     nr[K/mnt/testfs/l2: /mnt/testfs/l1: already migrated ...
     nr[K/mnt/testfs/l3: /mnt/testfs/l1: already migrated ...

Print out the "pool=" and "mirror_count=" parameters only if needed.

Fix "-A" option to round up the number of stripes when the migrated.
Skip sanity test_56xc 1GB test if there is not enough space on OSTs.

Fixes: 60c5bc2502 ("LU-8235 scripts: pass unrecognized options to lfs migrate")
Fixes: 80a2ff7137 ("LU-6051 utils: allow lfs_migrate to handle hard links")
Fixes: 99d7a8ed43 ("LU-8207 scripts: add auto-stripe option to lfs_migrate")
Test-Parameters: trivial fstype=zfs testlist=sanity envdefinitions=ONLY=56
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I059e7daeb2fa82e7607fd9d862797433053ebbe5

lustre/scripts/lfs_migrate
@@ -27,7 +27,7 @@ add_to_set() {
 	local old_fid="$1"
 	local path="$2"
 
-	echo -e "$old_fid $path" >> "$MIGRATED_SET"
+	echo "$old_fid $path" >> "$MIGRATED_SET"
 }
 
 path_in_set() {
@@ -139,7 +139,7 @@ while [ -n "$*" ]; do
 	-q|--quiet) ECHO=:;;
 	-R|--restripe) OPT_RESTRIPE=true;;
 	-s|--skip) OPT_CHECK=false;;
-	-v|--verbose) OPT_DEBUG=true; ECHO=echo; OPT_PASSTHROUGH+=("$arg");;
+	-v|--verbose) OPT_DEBUG=true; ECHO=echo;;
 	-y|--yes) OPT_YES=true;;
 	-0) OPT_NULL=true;;
 	-b|--block|--non-block|--non-direct|--no-verify)
@@ -256,7 +256,7 @@ function calc_stripe()
 			return
 		fi
 
-		if [ "$ost_min_kb" -eq $((1 << 62)) ]; then
+		if (( ost_min_kb == (1 << 62) )); then
 			echo "warning: unable to determine minimum OST size, " \
 			     "object size not capped" >&2
 			obj_max_kb=0
@@ -272,9 +272,10 @@ function calc_stripe()
 		return
 	fi
 
-	# If disk usage would exceed the cap, increase the number of stripes
-	[ $filekb -gt $((stripe_count * $obj_max_kb)) ] &&
-		stripe_count=$((filekb / $obj_max_kb))
+	# If disk usage would exceed the cap, increase the number of stripes.
+	# Round up to the nearest MB to ensure file will fit.
+	(( filekb > stripe_count * obj_max_kb )) &&
+		stripe_count=$(((filekb + obj_max_kb - 1024) / obj_max_kb))
 
 	# Limit the count to the number of eligible OSTs
 	if [ "$stripe_count" -gt $ost_max_count ]; then
@@ -304,13 +305,13 @@ lfs_migrate() {
 		# skip non-regular files, since they don't have any objects
 		# and there is no point in trying to migrate them.
 		if [ "${nlink_type[1]}" != "regular" ]; then
-			echo -e "$OLDNAME: not a regular file, skipped" 1>&2
+			echo -e "\r$OLDNAME: not a regular file, skipped" 1>&2
 			continue
 		fi
 
 		# working out write perms is hard, let the shell do it
 		if [ ! -w "$OLDNAME" ]; then
-			echo -e "$OLDNAME: no write permission, skipped" 1>&2
+			echo -e "\r$OLDNAME: no write permission, skipped" 1>&2
 			continue
 		fi
 
@@ -323,7 +324,7 @@ lfs_migrate() {
 		# also absolute so that the names can be compared
 		local oldname_absolute=$(readlink -f "$OLDNAME")
 		if [ -z "$oldname_absolute" ]; then
-			echo -e "$OLDNAME: cannot resolve full path, skipped" 1>&2
+			echo -e "\r$OLDNAME: cannot resolve full path, skipped" 1>&2
 			continue
 		fi
 		OLDNAME=$oldname_absolute
@@ -334,17 +335,15 @@ lfs_migrate() {
 		# for detecting unlisted hard links could then be removed.
 		local fid=$($LFS path2fid "$OLDNAME" 2> /dev/null)
 		if [ $? -ne 0 ]; then
-			echo -n "\r\e[K$OLDNAME: cannot determine FID; skipping; "
-			echo "is this a Lustre file system?"
-			echo -e "$OLDNAME: cannot determine FID; skipping; " 1>&2
-			echo "is this a Lustre file system?" 1>&2
+			echo -e "\r$OLDNAME: cannot determine FID; skipping; " \
+				"is this a Lustre file system?" 1>&2
 			continue
 		fi
 
 		if [[ ${nlink_type[0]} -gt 1 ]] || $RSYNC_WITH_HLINKS; then
 			# don't migrate a hard link if it was already migrated
 			if path_in_set "$OLDNAME"; then
-				$ECHO "\r\e[Kalready migrated via another hard link"
+				$ECHO "already migrated via another hard link"
 				continue
 			fi
 
@@ -355,7 +354,7 @@ lfs_migrate() {
 			# being migrated.
 			local migrated=$(old_fid_in_set "$fid")
 			if [ -n "$migrated" ]; then
-				$ECHO -e "$OLDNAME: already migrated via another hard link"
+				$ECHO "already migrated via another hard link"
 				if $OPT_RSYNC; then
 					# Only the rsync case has to relink.
 					# The lfs migrate case preserves the
@@ -383,6 +382,7 @@ lfs_migrate() {
 
 			if $OPT_AUTOSTRIPE; then
 				local filekb=$((${nlink_type[3]} / 1024))
+
 				read stripe_count OBJ_MAX_KB < <(calc_stripe \
 					"$OLDNAME" "$filekb" "$OBJ_MAX_KB")
 				[ -z "$stripe_count" ] && exit 1
@@ -417,15 +417,16 @@ lfs_migrate() {
 					       /dev/null)
 			fi
 
-			$ECHO -n "stripe" \
-				"count=${stripe_count:-$parent_count}," \
-				"size=${stripe_size:-$parent_size}," \
-				"pool=${stripe_pool}," \
-				"mirror_count=${mirror_count}"
+			$ECHO -n "stripe_count=${stripe_count:-$parent_count},stripe_size=${stripe_size:-$parent_size}"
+			[ -n "$stripe_pool" ] &&
+				$ECHO -n ",pool=${stripe_pool}"
+			[ -n "$mirror_count" ] &&
+				$ECHO -n ",mirror_count=${mirror_count}"
+			$ECHO -n " "
 		fi
 
 		if $OPT_DRYRUN; then
-			$ECHO "dry run, skipped"
+			$ECHO " dry run, skipped"
 			continue
 		fi
 
@@ -441,12 +442,12 @@ lfs_migrate() {
 		local mntpoint=$(df -P "$OLDNAME" |
 				awk 'NR==2 { print $NF; exit }')
 		if [ -z "$mntpoint" ]; then
-			echo -e "$OLDNAME: cannot determine mount point; skipped" 1>&2
+			echo -e "\r$OLDNAME: cannot determine mount point; skipped" 1>&2
 			continue
 		fi
 		hlinks=$($LFS fid2path "$mntpoint" "$fid" 2> /dev/null)
 		if [ $? -ne 0 ]; then
-			echo -e "$OLDNAME: cannot determine hard link paths, skipped" 1>&2
+			echo -e "\r$OLDNAME: cannot determine hard link paths, skipped" 1>&2
 			continue
 		fi
 		hlinks+=("$OLDNAME")
@@ -455,13 +456,13 @@ lfs_migrate() {
 		if ! $OPT_RSYNC; then
 			if $LFS migrate "${OPT_PASSTHROUGH[@]}" $layout \
 			   "$OLDNAME"; then
-				$ECHO "done migrate"
+				$ECHO "done"
 				for link in ${hlinks[*]}; do
 					add_to_set "$fid" "$link"
 				done
 				continue
 			elif $OPT_NO_RSYNC; then
-				echo -e "$OLDNAME: refusing to fall back to rsync, skipped" 1>&2
+				echo -e "\r$OLDNAME: refusing to fall back to rsync, skipped" 1>&2
 				continue
 			else
 				$ECHO -n "falling back to rsync: "
@@ -471,35 +472,35 @@ lfs_migrate() {
 
 		NEWNAME=$(mktemp $UNLINK "$OLDNAME-lfs_migrate.tmp.XXXXXX")
 		if [ $? -ne 0 -o -z "$NEWNAME" ]; then
-			echo -e "$OLDNAME: cannot make temp file, skipped" 1>&2
+			echo -e "\r$OLDNAME: cannot make temp file, skipped" 1>&2
 			continue
 		fi
 
 		if [ "$UNLINK" ]; then
 			if ! $LFS setstripe "${OPT_PASSTHROUGH[@]}" $layout \
 			     "$NEWNAME"; then
-				echo -e "\r\e[K$NEWNAME: setstripe failed, exiting" 1>&2
+				echo -e "\r$NEWNAME: setstripe failed, exiting" 1>&2
 				exit 2
 			fi
 		fi
 
 		# we use --inplace, since we created our own temp file already
 		if ! $RSYNC -a --inplace $RSYNC_OPTS "$OLDNAME" "$NEWNAME";then
-			echo -e "$OLDNAME: copy error, exiting" 1>&2
+			echo -e "\r$OLDNAME: copy error, exiting" 1>&2
 			exit 4
 		fi
 
 		if $OPT_CHECK && ! cmp -s "$OLDNAME" "$NEWNAME"; then
-			echo -e "$NEWNAME: compare failed, exiting" 1>&2
+			echo -e "\r$NEWNAME: compare failed, exiting" 1>&2
 			exit 8
 		fi
 
 		if ! mv "$NEWNAME" "$OLDNAME"; then
-			echo -e "$OLDNAME: rename error, exiting" 1>&2
+			echo -e "\r$OLDNAME: rename error, exiting" 1>&2
 			exit 12
 		fi
 
-		$ECHO "done migrate via rsync"
+		$ECHO "done rsync"
 		for link in ${hlinks[*]}; do
 			if [ "$link" != "$OLDNAME" ]; then
 				ln -f "$OLDNAME" "$link"

lustre/tests/sanity.sh
@@ -6312,7 +6312,7 @@ check_migrate_links() {
 	# make sure hard links were properly detected, and migration was
 	# performed only once for the entire link set; nonlinked files should
 	# also be migrated
-	local actual=$(grep -c 'done migrate' <<< "$migrate_out")
+	local actual=$(grep -c 'done' <<< "$migrate_out")
 	local expected=$(($uniq_count + 1))
 
 	[ "$actual" -eq  "$expected" ] ||
@@ -6363,13 +6363,14 @@ test_56xc() {
 
 	# Test 1: ensure file < 1 GB is always migrated with 1 stripe
 	echo -n "Setting initial stripe for 20MB test file..."
-	$LFS setstripe -c 2 -i 0 "$dir/20mb" || error "cannot setstripe"
+	$LFS setstripe -c 2 -i 0 "$dir/20mb" ||
+		error "cannot setstripe 20MB file"
 	echo "done"
 	echo -n "Sizing 20MB test file..."
 	truncate "$dir/20mb" 20971520 || error "cannot create 20MB test file"
 	echo "done"
 	echo -n "Verifying small file autostripe count is 1..."
-	$LFS_MIGRATE -y -A -C 1 "$dir/20mb" &> /dev/null ||
+	$LFS_MIGRATE -y -A -C 1 "$dir/20mb" ||
 		error "cannot migrate 20MB file"
 	local stripe_count=$($LFS getstripe -c "$dir/20mb") ||
 		error "cannot get stripe for $dir/20mb"
@@ -6382,23 +6383,27 @@ test_56xc() {
 	# sqrt(size_in_gb) + 1 OSTs but is larger than 1GB.  The file must
 	# have at least an additional 1KB for each desired stripe for test 3
 	echo -n "Setting stripe for 1GB test file..."
-	$LFS setstripe -c 1 -i 0 "$dir/1gb" || error "cannot setstripe"
+	$LFS setstripe -c 1 -i 0 "$dir/1gb" || error "cannot setstripe 1GB file"
 	echo "done"
 	echo -n "Sizing 1GB test file..."
 	# File size is 1GB + 3KB
-	truncate "$dir/1gb" 1073744896 &> /dev/null ||
-		error "cannot create 1GB test file"
+	truncate "$dir/1gb" 1073744896 || error "cannot create 1GB test file"
 	echo "done"
-	echo -n "Migrating 1GB file..."
-	$LFS_MIGRATE -y -A -C 1 "$dir/1gb" &> /dev/null ||
-		error "cannot migrate file"
-	echo "done"
-	echo -n "Verifying autostripe count is sqrt(n) + 1..."
-	stripe_count=$($LFS getstripe -c "$dir/1gb") ||
-		error "cannot get stripe for $dir/1gb"
-	[ $stripe_count -eq 2 ] ||
-		error "unexpected stripe count $stripe_count (expected 2)"
-	echo "done"
+
+	# need at least 512MB per OST for 1GB file to fit in 2 stripes
+	local avail=$($LCTL get_param -n llite.$FSNAME*.kbytesavail)
+	if (( avail > 524288 * OSTCOUNT )); then
+		echo -n "Migrating 1GB file..."
+		$LFS_MIGRATE -y -A -C 1 "$dir/1gb" ||
+			error "cannot migrate 1GB file"
+		echo "done"
+		echo -n "Verifying autostripe count is sqrt(n) + 1..."
+		stripe_count=$($LFS getstripe -c "$dir/1gb") ||
+			error "cannot getstripe for 1GB file"
+		[ $stripe_count -eq 2 ] ||
+			error "unexpected stripe count $stripe_count != 2"
+		echo "done"
+	fi
 
 	# Test 3: File is too large to fit within the available space on
 	# sqrt(n) + 1 OSTs.  Simulate limited available space with -X
@@ -6407,15 +6412,15 @@ test_56xc() {
 		# file size (1GB + 3KB) / OST count (3).
 		local kb_per_ost=349526
 
-		echo -n "Migrating 1GB file..."
-		$LFS_MIGRATE -y -A -C 1 -X $kb_per_ost "$dir/1gb" &>> \
-			/dev/null || error "cannot migrate file"
+		echo -n "Migrating 1GB file with limit..."
+		$LFS_MIGRATE -y -A -C 1 -X $kb_per_ost "$dir/1gb" ||
+			error "cannot migrate 1GB file with limit"
 		echo "done"
 
 		stripe_count=$($LFS getstripe -c "$dir/1gb")
-		echo -n "Verifying autostripe count with limited space..."
-		[ "$stripe_count" -a $stripe_count -eq 3 ] ||
-			error "unexpected stripe count $stripe_count (wanted 3)"
+		echo -n "Verifying 1GB autostripe count with limited space..."
+		[ "$stripe_count" -a $stripe_count -ge 3 ] ||
+			error "unexpected stripe count $stripe_count (min 3)"
 		echo "done"
 	fi
 
@@ -6466,18 +6471,20 @@ test_56z() { # LU-4824
 	test_mkdir $dir
 	for i in d{0..9}; do
 		test_mkdir $dir/$i
+		touch $dir/$i/$tfile
 	done
-	touch $dir/d{0..9}/$tfile
 	$LFS find $DIR/non_existent_dir $dir &&
 		error "$LFS find did not return an error"
 	# Make a directory unsearchable. This should NOT be the last entry in
 	# directory order.  Arbitrarily pick the 6th entry
 	chmod 700 $($LFS find $dir -type d | sed '6!d')
 
+	$RUNAS $LFS find $DIR/non_existent $dir
 	local count=$($RUNAS $LFS find $DIR/non_existent $dir | wc -l)
 
 	# The user should be able to see 10 directories and 9 files
-	[ $count == 19 ] || error "$LFS find did not continue after error"
+	(( count == 19 )) ||
+		error "$LFS find found $count != 19 entries after error"
 }
 run_test 56z "lfs find should continue after an error"
 